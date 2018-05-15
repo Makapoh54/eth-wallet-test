3525791af9495eee9ebb05288284040e8d85abd9
unknown <makapoh54@gmail.com>
2018-05-16 00:53:05 +0300

    IP address change

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -3,7 +3,7 @@ const ROUTES = {
 }
 
 const TABLE_HEADERS = ['#', 'Account Address', 'Eth Balance', 'Token Balance', 'Transfer'];
-const END_POINT = 'http://35.204.88.148/api/v1';
+const END_POINT = 'http://35.204.150.202/api/v1';
 const TRANSFER_TYPES = ['eth', 'token'];
 
 export { ROUTES, TABLE_HEADERS, END_POINT, TRANSFER_TYPES };
